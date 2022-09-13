526ebdd521657fd67d04b981408eae515af301cb
naitikchau <104361541+naitikchau@users.noreply.github.com>
2022-09-13 22:52:36 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+function startClassification()
+{
+    navigator.mediaDevices.getUserMedia({audio:true});
+    classifier = ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/XSipLvQmK/model.json',modelReady);
+}
+
+function modelReady()
+{
+    classifier.classify(gotResult);
+}
+
+function gotResult(error, results)
+{
+    if(error)
+    {
+        console.error(error);
+    }
+    else
+    {
+        console.log(results);
+    }
+}
